6501d8b49cabfbe4b7024e587b37df3b883e1aa2
Denys07748 <denis07748@gmail.com>
2023-01-23 20:37:30 +0200

    додає task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainerEl = document.querySelector(".gallery");
+const imegesMarkup = createImegesMarkup(galleryItems);
+
+galleryContainerEl.insertAdjacentHTML("beforeend", imegesMarkup);
+
+function createImegesMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) => `
+  <li>
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+  </li>`
+    )
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
